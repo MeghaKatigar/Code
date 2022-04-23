543c414836a41d0015be816500e6a4db759bc5b5
Megha katigar <98883965+MeghaKatigar@users.noreply.github.com>
2022-04-23 11:51:50 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  int a,b,c,d;
+  a=10, b=20;
+  c=sum(a,b);
+  d=square(a);
+  printf("Sum of a & b is %d", c);
+  printf("Square of a is %d", d);
+  return 0;
+}
